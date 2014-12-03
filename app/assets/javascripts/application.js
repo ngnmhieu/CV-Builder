144abfb7d780495a849957a0484f7f5fc242dd84
Hieu M. Nguyen <ngnmhieu@gmail.com>
2014-12-03 18:55:45 +0100

    Add Bootstrap

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -14,3 +14,35 @@
 //= require jquery_ujs
 //= require turbolinks
 //= require_tree .
+
+ResumeEditor = (function() {
+    return {
+        /**
+         * initialize object
+         *
+         */
+        init: function() {
+            // hide all the edit sections
+            $('.sec').hide();
+
+            this.initEvents();
+        },
+
+        initEvents: function() {
+            $('.sec-tab').on('click', function() {
+                var tab = $(this);
+                var order = tab.attr('data-order');
+                $('.sec-tab').removeClass('active');
+                tab.addClass('active');
+                $('.sec').hide();
+                $('.sec[data-order='+order+']').show();
+            });
+        },
+    }
+})();
+
+(function($) {
+    $(document).ready(function(){
+        ResumeEditor.init();
+    });
+})(jQuery);
