b9806b5d4b3e987225ca14170f9b381aa49a7b62
Hieu M. Nguyen <ngnmhieu@gmail.com>
2014-12-05 19:07:11 +0100

    rich editor

--- app/assets/javascripts/editor.js
+++ app/assets/javascripts/editor.js
@@ -8,7 +8,20 @@ ResumeEditor = (function() {
         },
 
         initRichEditors: function() {
-            $('.textsection textarea').wysihtml5();
+            $('.textsection textarea').wysihtml5({
+                toolbar: {
+                    "font-styles": false, //Font styling, e.g. h1, h2, etc. Default true
+                    "emphasis": true, //Italics, bold, etc. Default true
+                    "lists": true, //(Un)ordered lists, e.g. Bullets, Numbers. Default true
+                    "html": false, //Button which allows you to edit the generated HTML. Default false
+                    "link": true, //Button to insert a link. Default true
+                    "image": false, //Button to insert an image. Default true,
+                    "color": false, //Button to change color of font  
+                    "blockquote": false, //Blockquote  
+                    "Indent": false
+                }
+            });
+
             $('.multilinelist textarea').wysihtml5();
             $('.simplelist textarea').wysihtml5();
         },
