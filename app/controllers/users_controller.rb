286b1c0322ddde1a9a74782843d41f52e12d2de7
ngnmhieu <ngnmhieu@gmail.com>
2014-12-27 12:51:49 +0100

    add user skeleton

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,10 @@
+class UsersController < ApplicationController
+  def login
+  end
+  
+  def logout
+  end
+
+  def register
+  end
+end
---
+++ db/migrate/20141227114902_create_users.rb
@@ -0,0 +1,11 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :name
+      t.string :email, null: false
+      t.string :password, null: false
+
+      t.timestamps
+    end
+  end
+end
