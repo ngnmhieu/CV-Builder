b1a9fd2741c49d43e7a559a35e4e9ea28bfba3a6
ngnmhieu <ngnmhieu@gmail.com>
2014-12-31 01:10:43 +0100

    Styling

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,4 +4,14 @@ class User < ActiveRecord::Base
   has_secure_password
   validates :email, presence: true, uniqueness: true, length: { maximum: 255 },
                     format: /\A([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})\z/i
+
+
+  before_create :default_attributes
+
+  def default_attributes
+    if self.email
+      self.name ||= self.email.split('@')[0].capitalize
+    end
+  end
+
 end
