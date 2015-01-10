0bc81fdbb80a46460841861c38df78d47a7f77ca
ngnmhieu <ngnmhieu@gmail.com>
2015-01-10 22:07:35 +0100

    add some more test for users_controller.rb

--- spec/controllers/users_controller_spec.rb
+++ spec/controllers/users_controller_spec.rb
@@ -1,8 +1,9 @@
 require 'rails_helper'
 
 describe UsersController, :type => :controller do
-  describe "User login" do
-    let(:params) { { email: "test@chocolate.sweet", password: "eat" } }
+  describe "User Login" do
+    let(:params) { { email: "test@eat.chocolate", password: "eat" } }
+    let(:test_user) { double('user', id: 10, name: 'test', email: 'test@eat.chocolate', password: '1234') }
 
     it "should authenticate user" do
       expect(User).to receive(:authenticate).with(params[:email], params[:password])
@@ -10,20 +11,68 @@ describe UsersController, :type => :controller do
     end
 
     it "should return to homepage when login succeed" do
-      user = double('user', id: 10, name: 'test', email: 'test@eat.chocolate')
-      # assume authenticate successfull
-      allow(User).to receive(:authenticate) { user }
+      # assume authentication succeeds
+      allow(User).to receive(:authenticate) { test_user }
       post :authenticate, { user: params }
 
       expect(response).to redirect_to(root_path)
     end
 
     it "should return to login page when login fails" do
+      # assume authentication fails
       allow(User).to receive(:authenticate) { false }
       post :authenticate, { user: params }
 
       expect(response).to redirect_to(login_path)
     end
 
+    it "should return to homepage if user already logged in" do
+      allow(controller).to receive(:logged_in?) { true }
+      get :login 
+
+      expect(response).to redirect_to(root_path)
+    end
   end
+
+  describe "User Logout" do
+    it "should actually logs user out" do
+    end
+
+    it "should return to hompage after logging out" do
+      post :logout
+
+      expect(response).to redirect_to(root_path)
+    end
+  end
+
+  describe "User Registration" do
+    let(:test_user) { double('user', id: 10, name: 'test', email: 'test@eat.chocolate', password: '1234') }
+    let(:register_params) { { user: { email: test_user.email, password: test_user.password } } }
+
+    it "should save new user" do
+      allow(User).to receive(:new) { test_user }
+      expect(test_user).to receive(:save)
+
+      post :create, register_params
+    end
+
+    it "should return to homepage on success" do
+      allow(User).to receive(:new) { test_user }
+      allow(test_user).to receive(:save) { true }
+
+      post :create, register_params
+
+      expect(response).to redirect_to(root_path) 
+    end
+
+    it "should return to registration page on failure" do
+      allow(User).to receive(:new) { test_user }
+      allow(test_user).to receive(:save) { false }
+
+      post :create, register_params 
+
+      expect(response).to render_template(:register) 
+    end
+  end
+
 end
