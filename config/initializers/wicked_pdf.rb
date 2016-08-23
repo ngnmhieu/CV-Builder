16158fe786bb09abffa17ddfef9a9072cd9b1cff
ngnmhieu <ngnmhieu@gmail.com>
2016-08-23 23:47:42 +0200

    change wickedpdf config

--- config/initializers/wicked_pdf.rb
+++ config/initializers/wicked_pdf.rb
@@ -1,4 +1,9 @@
+if Rails.env.production?
+  wkhtmltopdf_path = Rails.root.join('vendor/bundle/bin/wkhtmltopdf').to_s 
+else
+  wkhtmltopdf_path = "/usr/local/bin/wkhtmltopdf"            
+end
+
 WickedPdf.config = {
-  # :exe_path => Rails.root.join('bin/wkhtmltopdf').to_s
-  :exe_path => '/usr/local/bin/wkhtmltopdf'
+  :exe_path => wkhtmltopdf_path
 }
